87193b00e9b5886aefc2c69fe02a6944217c365b
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 00:18:47 +0900

    色を表現してみよう

--- front/main.js
+++ front/main.js
@@ -4,10 +4,11 @@ const sketch = (p) => {
   p.setup = () => {
     //最初に１回だけ実行される処理
     p.createCanvas(480, 240);
+    p.background("rgba(255,0,0,0.5)");
   };
   p.draw = () => {
     //そのあと繰り返し実行される処理
-    p.background(0);
+    // p.background(0);
     p.rect(x, 40, 50, 50);
     x++;
   };
