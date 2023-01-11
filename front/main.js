c902aeb9b0fc3c59b851590e7d5bf9e0111cad89
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 00:28:20 +0900

    ellipse()やline()を使ってみよう

--- front/main.js
+++ front/main.js
@@ -3,11 +3,12 @@ let x = 0;
 const sketch = (p) => {
   p.setup = () => {
     //最初に１回だけ実行される処理
-    // rect(x,y,w,h)
     p.createCanvas(480, 240);
     p.background("skyblue");
-    p.rectMode(p.CENTER);
+    p.rectMode(p.LEFT);
     p.rect(p.width / 2, p.height / 2, 150, 100);
+    p.ellipse(p.width / 2, p.height / 2, 100, 100);
+    p.line(0, 0, p.width / 2, p.height / 2);
   };
   // p.draw = () => {
   //   //そのあと繰り返し実行される処理
