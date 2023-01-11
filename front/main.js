70f995dfa3d950372f1fcb715f662731786dffd8
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 00:33:46 +0900

    塗と線の設定をしてみよう

--- front/main.js
+++ front/main.js
@@ -3,11 +3,16 @@ let x = 0;
 const sketch = (p) => {
   p.setup = () => {
     //最初に１回だけ実行される処理
-    p.createCanvas(480, 240);
+    p.createCanvas(480, 480);
     p.background("skyblue");
     p.rectMode(p.LEFT);
+    p.noFill();
     p.rect(p.width / 2, p.height / 2, 150, 100);
+    const c = p.color("pink");
+    p.fill(c);
     p.ellipse(p.width / 2, p.height / 2, 100, 100);
+    p.stroke("blue");
+    p.strokeWeight(5);
     p.line(0, 0, p.width / 2, p.height / 2);
   };
   // p.draw = () => {
