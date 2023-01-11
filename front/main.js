b32ea83d3d9be94e5cbb77f54c14fd111675afc8
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 00:46:46 +0900

    テキストを描画していよう

--- front/main.js
+++ front/main.js
@@ -3,17 +3,29 @@ let x = 0;
 const sketch = (p) => {
   p.setup = () => {
     //最初に１回だけ実行される処理
-    p.createCanvas(480, 480);
+    p.createCanvas(p.windowWidth, p.windowHeight);
     p.background("skyblue");
+
+    // 長方形
     p.rectMode(p.LEFT);
     p.noFill();
     p.rect(p.width / 2, p.height / 2, 150, 100);
     const c = p.color("pink");
+    // 円
     p.fill(c);
     p.ellipse(p.width / 2, p.height / 2, 100, 100);
+
+    // 線
     p.stroke("blue");
     p.strokeWeight(5);
     p.line(0, 0, p.width / 2, p.height / 2);
+    // テキスト
+    p.textAlign(p.LEFT, p.TOP);
+    p.noStroke();
+    p.textSize(32);
+    p.textFont("impact");
+    p.textStyle(p.ITALIC);
+    p.text("Hello World", 100, 100);
   };
   // p.draw = () => {
   //   //そのあと繰り返し実行される処理
