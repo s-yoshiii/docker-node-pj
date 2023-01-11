c5f2ff0ac5f1d0b642afd54283a45b318f346ca4
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 00:24:41 +0900

    rect()で長方形を描いてみよう

--- front/main.js
+++ front/main.js
@@ -3,15 +3,17 @@ let x = 0;
 const sketch = (p) => {
   p.setup = () => {
     //最初に１回だけ実行される処理
+    // rect(x,y,w,h)
     p.createCanvas(480, 240);
-    p.background("rgba(255,0,0,0.5)");
+    p.background("skyblue");
+    p.rectMode(p.CENTER);
+    p.rect(p.width / 2, p.height / 2, 150, 100);
   };
-  p.draw = () => {
-    //そのあと繰り返し実行される処理
-    // p.background(0);
-    p.rect(x, 40, 50, 50);
-    x++;
-  };
+  // p.draw = () => {
+  //   //そのあと繰り返し実行される処理
+  //   p.rect(x, 40, 50, 50);
+  //   x++;
+  // };
 };
 
 new p5(sketch);
