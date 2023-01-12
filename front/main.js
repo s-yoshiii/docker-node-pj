bf229f81926325982ca77cc41b20735565f1c87d
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-12 10:35:21 +0900

    図形を変形させてみよう

--- front/main.js
+++ front/main.js
@@ -7,26 +7,17 @@ const sketch = (p) => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background("skyblue");
 
-    // 長方形
-    p.rectMode(p.LEFT);
-    p.noFill();
-    p.rect(p.width / 2, p.height / 2, 150, 100);
-    const c = p.color("pink");
-    // 円
-    p.fill(c);
-    p.ellipse(p.width / 2, p.height / 2, 100, 100);
+    p.fill(255, 0, 0, 127);
+    p.rect(0, 0, 100, 100);
 
-    // 線
-    p.stroke("blue");
-    p.strokeWeight(5);
-    p.line(0, 0, p.width / 2, p.height / 2);
-    // テキスト
-    p.textAlign(p.LEFT, p.TOP);
-    p.noStroke();
-    p.textSize(32);
-    p.textFont("impact");
-    p.textStyle(p.ITALIC);
-    p.text("Hello World", 100, 100);
+    p.push();
+    // p.translate(10, 10);
+    // p.rotate(p.PI / 4);
+    // p.rotate(p.radians(30));
+    p.scale(2, 0.5);
+    p.fill(0, 0, 255, 127);
+    p.rect(0, 0, 100, 100);
+    p.pop();
   };
   // p.draw = () => {
   //   //そのあと繰り返し実行される処理
