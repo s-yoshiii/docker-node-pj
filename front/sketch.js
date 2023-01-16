7714823d8bb472571ff373f801687101d49dc390
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-16 18:31:01 +0900

    グリッドを作ってみよう

--- front/sketch.js
+++ front/sketch.js
@@ -9,6 +9,17 @@ const sketch = (p) => {
     p.background(0);
     p.noStroke();
     p.fill(255);
+
+    const size = 5;
+    p.ellipse(50, 50, size, size);
+    p.ellipse(100, 50, size, size);
+    p.ellipse(150, 50, size, size);
+    p.ellipse(50, 100, size, size);
+    p.ellipse(100, 100, size, size);
+    p.ellipse(150, 100, size, size);
+    p.ellipse(50, 150, size, size);
+    p.ellipse(100, 150, size, size);
+    p.ellipse(150, 150, size, size);
   };
 };
 new p5(sketch);
