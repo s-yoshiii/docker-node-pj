609a5c5bbc191f112a2372fa81a9fbe1c54eb4da
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-18 16:27:53 +0900

    乱数を使ってみよう

--- front/sketch.js
+++ front/sketch.js
@@ -10,10 +10,16 @@ const sketch = (p) => {
     p.noStroke();
     p.fill(255);
 
-    const size = 5;
     const step = 20;
     for (let x = 0; x <= p.width; x += step) {
       for (let y = 0; y <= p.height; y += step) {
+        // const size = p.random(0, 15);
+        let size;
+        if (p.random() < 0.95) {
+          size = p.random(0, 10);
+        } else {
+          size = step;
+        }
         p.ellipse(x, y, size, size);
       }
     }
