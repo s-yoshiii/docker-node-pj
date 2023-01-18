da8d1391f68380ec9ce4237e1cf36c08e6f1857f
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-18 16:58:22 +0900

    作品に動きをつけてみよう

--- front/sketch.js
+++ front/sketch.js
@@ -6,22 +6,20 @@ const sketch = (p) => {
     // 描画領域のセットアップ
     let canvas = p.createCanvas(600, 200);
     canvas.parent("sketch");
-    p.background(0);
     p.noStroke();
     p.fill(255);
-
+    p.colorMode(p.HSB, 360, 100, 100, 255);
+  };
+  p.draw = () => {
+    p.background(0, 20);
     const step = 20;
-    p.colorMode(p.HSB, 360, 100, 100);
+    const size = 5;
     for (let x = 0; x <= p.width; x += step) {
       for (let y = 0; y <= p.height; y += step) {
-        const size = p.random(0, 15);
-        const h = p.random(200, 320);
-        const s = 100;
-        const b = size * 5;
-        p.fill(h, s, b);
         p.ellipse(x, y, size, size);
       }
     }
+    p.ellipse(p.mouseX, p.mouseY, 20, 20);
   };
 };
 new p5(sketch);
