228423adf15178c826e276c6e5424de4138c4e9b
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-18 16:06:13 +0900

    円の数を増やしてみよう

--- front/sketch.js
+++ front/sketch.js
@@ -4,15 +4,16 @@ import "reset-css";
 const sketch = (p) => {
   p.setup = () => {
     // 描画領域のセットアップ
-    let canvas = p.createCanvas(200, 200);
+    let canvas = p.createCanvas(600, 200);
     canvas.parent("sketch");
     p.background(0);
     p.noStroke();
     p.fill(255);
 
     const size = 5;
-    for (let x = 50; x <= 150; x += 50) {
-      for (let y = 50; y <= 150; y += 50) {
+    const step = 20;
+    for (let x = 0; x <= p.width; x += step) {
+      for (let y = 0; y <= p.height; y += step) {
         p.ellipse(x, y, size, size);
       }
     }
