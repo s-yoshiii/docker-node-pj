9dc700d7efeb1151142c21c3c618614b0c420460
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-21 21:52:07 +0900

    すべての要素に処理を設定しよう。

--- front/script.js
+++ front/script.js
@@ -1,18 +1,22 @@
 "use strict";
 {
-  const target = document.querySelector(".js-fade-in");
+  const targets = [...document.querySelectorAll(".js-fade-in")];
   const motion = (entries, obs) => {
-    console.log(entries[0]);
-    if (!entries[0].isIntersecting) {
-      return;
-    }
-    entries[0].target.classList.add("is-visible");
-    obs.unobserve(entries[0].target);
+    console.log(entries);
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      entry.target.classList.add("is-visible");
+      obs.unobserve(entry.target);
+    });
   };
   const options = {
     threshold: 0.2,
     // rootMargin: "0px 0px -100px",
   };
   const observer = new IntersectionObserver(motion, options);
-  observer.observe(target);
+  targets.forEach((target) => {
+    observer.observe(target);
+  });
 }
