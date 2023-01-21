0ecd901ed3e3568168ace4a3875a0006a5c323e4
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-21 21:41:48 +0900

    targetの監視を止めてみよう

--- front/script.js
+++ front/script.js
@@ -1,15 +1,17 @@
 "use strict";
 {
   const target = document.querySelector(".js-fade-in");
-  const motion = (entries) => {
+  const motion = (entries, obs) => {
+    console.log(entries[0]);
     if (!entries[0].isIntersecting) {
       return;
     }
     entries[0].target.classList.add("is-visible");
+    obs.unobserve(entries[0].target);
   };
   const options = {
-    threshold: 1,
-    rootMargin: "0px 0px -100px",
+    threshold: 0.2,
+    // rootMargin: "0px 0px -100px",
   };
   const observer = new IntersectionObserver(motion, options);
   observer.observe(target);
